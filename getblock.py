08c08781a8b903d91e7ed1203fd94eb766bf1b9c
John Davies <jd.other@hotmail.com>
2022-12-05 20:18:56 +0000

    implemented getblock handler

--- getblock.py
+++ getblock.py
@@ -9,9 +9,13 @@ USERNAME = os.environ["SSH_USER"]
 PASSWORD = os.environ["SSH_PASSWORD"]
 
 def lambda_handler(event, context): 
+    # print(f"event queryStringParameters: {event['queryStringParameters']}")
+    # print(f"event: {event}")
+    
     # print('event block hash: ', event['hash'])
     # myhash = event['hash']
-    myhash = "00000000c937983704a73af28acdec37b049d214adbda81d7e2a3dd146f6ed09"
+    # myhash = "00000000c937983704a73af28acdec37b049d214adbda81d7e2a3dd146f6ed09"
+    myhash = event['queryStringParameters']['hash']
     
     client = paramiko.client.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -19,14 +23,31 @@ def lambda_handler(event, context):
     # _stdin, _stdout,_stderr = client.exec_command(f"bitcoin-cli -named getblock blockhash={myhash} verbosity=0")
     _stdin, _stdout,_stderr = client.exec_command(f"bitcoin-cli -named getblock blockhash={myhash} verbosity=1")
     response = _stdout.read().decode()
+    # print('response: ', response)
     client.close()
     
     response_object = {}
     response_object['statusCode'] = 200
     response_object['headers'] = {}
     response_object['headers']['Content-type'] = 'application/json'
-    response_object['body'] = json.loads(response)
-    # response_object['body'] = json.dumps(response)
+    response_object['body'] = json.dumps(response)
+    
+    # print('response object: ', response_object)
     
     return response_object
+    
+    # print(f"event qsp: {event['queryStringParameters']}")
+    
+    # return {
+        # "statusCode": 200,
+        # "headers": {
+            # "Content-Type": "application/json"
+        # },
+        # 'body': json.dumps('Hello from Lambda!')
+        # "body": json.dumps({
+        #     "myBodyKey": "myBodyValue"
+        # })
+        # "body": json.dumps(response)
+        # "body": json.dumps(event['queryStringParameters'])
+    # }
 
